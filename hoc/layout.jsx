8f8e18a0518214257826e9e75614b74dad9c4f65
Arvind Narayan <thearvindnarayan@gmail.com>
2020-03-28 22:05:49 +0530

    refinement of SEO

--- hoc/layout.jsx
+++ hoc/layout.jsx
@@ -1,25 +1,25 @@
-import React from 'react'
-import Head from 'next/head'
+import React from 'react';
+import Head from 'next/head';
 
 // eslint-disable-next-line react/prefer-stateless-function
 class Layout extends React.Component {
-  sidebarToggle () {
-    document.querySelector(`body`).classList.toggle('sidebar-active')
-  }
-  render () {
+  render() {
     return (
       <React.Fragment>
         <Head>
-          <title>Arvind Narayan | Developer & Designer</title>
+          <title>Arvind Narayan | Developer, UX and Product Strategy</title>
           <link rel='icon' href='/static/favicon.ico' />
           <link
             href='https://fonts.googleapis.com/css?family=Oxygen:400,700|Zilla+Slab:700'
             rel='stylesheet'
           />
-          <meta name='title' content='Arvind Narayan | Developer & Desinger' />
+          <meta
+            name='title'
+            content='Arvind Narayan | Developer, UX and Product Strategy'
+          />
           <meta
             name='description'
-            content='Arvind Narayan Full-stack Develope and freelance UX Designer from Bengaluru, India. I specialise in React, Redux, Node js and sketch. I build Apps.'
+            content="Arvind Narayan is Developer, Freelance UX Designer and Product Strategist from Bangalore. He is a seasoned professional with experience in multiple domains. He's worked with startups to enterprises in providing solutions."
           />
           <meta
             name='keywords'
@@ -28,36 +28,42 @@ class Layout extends React.Component {
           <meta name='robots' content='index, follow' />
           <meta httpEquiv='Content-Type' content='text/html; charset=utf-8' />
           <meta name='language' content='English' />
-
           <meta
             name='viewport'
             content='width=device-width, initial-scale=1.0'
           />
           <meta
             property='og:site_name'
-            content='Arvind Narayan | Developer & Designer'
+            content='Arvind Narayan | Developer, UX and Product Strategy'
           />
           <meta property='og:url' content='https://www.pranaafood.com/' />
           <meta
             property='og:title'
             name='title'
-            content='Arvind Narayan | Developer & Designer'
+            content='Arvind Narayan | Developer, UX and Product Strategy'
           />
           <meta name='twitter:url' content='https://www.pranaafood.com/' />
           <meta
             name='twitter:title'
-            content='Arvind Narayan | Developer & Designer'
+            content='Arvind Narayan | Developer, UX and Product Strategy'
           />
           <meta
             name='twitter:site'
-            content='Arvind Narayan | Developer & Designer'
+            content='Arvind Narayan | Developer, UX and Product Strategy'
           />
           <meta name='twitter:creator' content='Arvind Narayan' />
           <meta property='og:type' content='website' />
           <meta name='theme-color' content='#3c4040' />
-          <meta property="og:image" content="https://avatars1.githubusercontent.com/u/26936172" />>
-          <meta property="twitter:image" content="https://avatars1.githubusercontent.com/u/26936172" />
-          <script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/110/three.js"></script>
+          <meta
+            property='og:image'
+            content='https://avatars1.githubusercontent.com/u/26936172'
+          />
+          >
+          <meta
+            property='twitter:image'
+            content='https://avatars1.githubusercontent.com/u/26936172'
+          />
+          <script src='https://cdnjs.cloudflare.com/ajax/libs/three.js/110/three.js'></script>
         </Head>
         <style jsx global>{`
           body {
@@ -66,8 +72,8 @@ class Layout extends React.Component {
         `}</style>
         {this.props.children}
       </React.Fragment>
-    )
+    );
   }
 }
 
-export default Layout
+export default Layout;
