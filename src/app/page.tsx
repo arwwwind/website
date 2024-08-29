60ebcb327a991877bd3970ee1c4c36e9752567f2
Arvind Narayan <thearvindnarayan@gmail.com>
2024-08-29 19:54:12 +0530

    v2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,181 @@
-import Image from "next/image";
+import { CardContainer, CardItem } from '@/components/ui/3d-card';
+import { Button } from '@/components/ui/button';
+import { FlipWords } from '@/components/ui/flip-words';
 
 export default function Home() {
+  const hello = ['Hi,', 'Hello,', 'Hallo,', 'Hola!', 'Bonjour,'];
+  const does = [
+    'Senior Engineer',
+    'Senior Architect',
+    'Freelance Developer',
+    'Problem Solver',
+    'Dog Dad!',
+  ];
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
+    <div className='min-h-[100vh] w-full dark:bg-black bg-white  dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative'>
+      {/* Radial gradient for the container to give a faded look */}
+      <div className='absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_50%,black)]'></div>
+      <div className='py-8 p-4 grid grid-cols-2 gap-4'>
+        <div className='avatar flex flex-col items-center'>
+          <CardContainer>
+            <CardItem translateZ='100' className='w-full mt-4'>
+              <img
+                className='inline-block h-[200px] w-[auto] md:h-[400px] rounded-full ring-[3px] ring-red-600'
+                src='/avatar.png'
+                alt='Arvind Narayan'
+              />
+            </CardItem>
+          </CardContainer>
+
+          <div className='py-2 pt-0 flex '>
+            <a
+              href='https://calendly.com/thearvindnarayan/15min'
+              target='_blank'
+              rel='noopener noreferrer'
+              className='px-2'
+            >
+              <button
+                type='button'
+                className='text-white bg-rose-800 hover:bg-red-700 ring-2 focus:outline-none ring-rose-400 font-medium rounded-lg text-sm px-4 py-2 text-center hover:bg-gradient-to-b from-rose-600 to-rose-900 transition ease-in-out flex items-center'
+              >
+                <i className='lni lni-google-meet px-1'></i>
+                <span>1:1 Chat</span>
+              </button>
+            </a>
+            <a
+              href='mailto:hi@arwwwind.com?subject=Hello%20Arvind%2C'
+              target='_blank'
+              rel='noopener noreferrer'
+              className='px-2'
+            >
+              <button
+                type='button'
+                className='text-white hover:text-rose-700 bg-neutral-800 hover:bg-neutral-900 ring-2 focus:outline-none ring-gray-500 hover:ring-rose-700 font-medium rounded-lg text-sm px-4 py-2 text-center ease-in-out flex items-center'
+              >
+                <i className='lni lni-envelope px-1'></i>
+                <span>Email</span>
+              </button>
+            </a>
+          </div>
+          <div className='py-2 flex'>
+            <a
+              href='https://www.linkedin.com/in/arwwwind/'
+              target='_blank'
+              rel='noopener noreferrer'
+              className='px-2'
+            >
+              <Button variant='outline'>
+                <i className='lni lni-linkedin-original px-1'></i>
+                <span>Connect</span>
+              </Button>
+            </a>
+            <a
+              href='https://github.com/arwwwind'
+              target='_blank'
+              rel='noopener noreferrer'
+              className='px-2'
+            >
+              <Button variant='outline'>
+                <i className='lni lni-github-original px-1'></i>
+                <span>Github</span>
+              </Button>
+            </a>
+          </div>
+        </div>
+        <div className='intro font-bold'>
+          <h1 className='text-3xl py-1'>
+            <FlipWords words={hello} />
+          </h1>
+          <h2 className='text-4xl px-2 py-1'>
+            <span>{`I'm`}</span>
+            <span className='text-rose-700 px-4'>Arvind Narayan</span>
+          </h2>
+          <h2 className='text-3xl'>
+            <FlipWords words={does} />
+          </h2>
+          <p className='p-2 pt-8 w-[60%] font-normal text-sm tracking-wide leading-relaxed'>
+            I’m an engineer with six years in backend and frontend development,
+            wrangling code and leading teams to build cool stuff. I’ve
+            turbocharged upGrad’s Learning Management System and revived Yahoo’s
+            ad builder, making them all shiny and user-friendly. At Egen, I’m
+            cooking up API magic and streamlining operations. My toolkit
+            includes Node, React.JS, Python, and AWS, but I’m always game for
+            whatever tech stack your project needs. When I’m not deep in code at
+            work, I’m on the lookout for fun freelance gigs. Need a tech lead or
+            a quick, crafty solution? I’m your guy, bringing skills and a
+            sprinkle of humor to the table.
+          </p>
+        </div>
+      </div>
+      <div className='flex flex-col w-[100%] p-4 items-center'>
+        <h4 className='text-center text-3xl font-bold'>Current Stack</h4>
+        <div className='flex py-4'>
+          <a href='https://www.typescriptlang.org/'>
+            <div
+              style={{ backgroundImage: 'url("/ts.svg")' }}
+              className='stack-item w-[50px] h-[50px] bg-contain bg-center saturate-0 hover:saturate-100 bg-no-repeat m-2'
+            >
+              <span className='hidden'>Typescript</span>
+            </div>
+          </a>
+          <a href='https://www.python.org/'>
+            <div
+              style={{ backgroundImage: 'url("/py.png")' }}
+              className='stack-item w-[50px] h-[50px] bg-contain bg-center saturate-0 hover:saturate-100 bg-no-repeat m-2'
+            >
+              <span className='hidden'>Python</span>
+            </div>
+          </a>
+          <a href='https://www.react.dev/'>
+            <div
+              style={{ backgroundImage: 'url("/react.png")' }}
+              className='stack-item w-[50px] h-[50px] bg-contain bg-center saturate-0 hover:saturate-100 bg-no-repeat m-2'
+            >
+              <span className='hidden'>React</span>
+            </div>
+          </a>
+          <a href='https://nodejs.org/'>
+            <div
+              style={{ backgroundImage: 'url("/node.png")' }}
+              className='stack-item w-[50px] h-[50px] bg-contain bg-center saturate-0 hover:saturate-100 bg-no-repeat m-2'
+            >
+              <span className='hidden'>Node</span>
+            </div>
+          </a>
+          <a href='https://aws.amazon.com/'>
+            <div
+              style={{ backgroundImage: 'url("/aws.png")' }}
+              className='stack-item w-[50px] h-[50px] bg-contain bg-center saturate-0 hover:saturate-100 bg-no-repeat m-2'
+            >
+              <span className='hidden'>AWS</span>
+            </div>
+          </a>
+          <a href='https://www.postgresql.org/'>
+            <div
+              style={{ backgroundImage: 'url("/pg.png")' }}
+              className='stack-item w-[50px] h-[50px] bg-contain bg-center saturate-0 hover:saturate-100 bg-no-repeat m-2'
+            >
+              <span className='hidden'>Postgresql</span>
+            </div>
+          </a>
+          <a href='https://redis.io/'>
+            <div
+              style={{ backgroundImage: 'url("/redis.svg")' }}
+              className='stack-item w-[50px] h-[50px] bg-contain bg-center saturate-0 hover:saturate-100 bg-no-repeat m-2'
+            >
+              <span className='hidden'>Redis</span>
+            </div>
+          </a>
+          <a href='https://www.mongodb.com/'>
+            <div
+              style={{ backgroundImage: 'url("/mongo.svg")' }}
+              className='stack-item w-[50px] h-[50px] bg-contain bg-center saturate-0 hover:saturate-100 bg-no-repeat m-2'
+            >
+              <span className='hidden'>Mongo</span>
+            </div>
           </a>
         </div>
       </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   );
 }
